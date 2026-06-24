bcdcff7d29862646e4a616615292e3db04975746
dk <kudin.dima@gmail.com>
2026-06-24 21:12:02 +0000

    Initial commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,74 @@
+import FooterMinimal from '@/components/sections/footer/FooterMinimal';
+import NavbarCentered from '@/components/ui/NavbarCentered';
+import SectionErrorBoundary from "@/components/ui/SectionErrorBoundary";
+import SiteBackgroundSlot from "@/components/ui/SiteBackgroundSlot";
+import { Instagram, Twitter } from "lucide-react";
+import { Outlet } from 'react-router-dom';
+import { StyleProvider } from "@/components/ui/StyleProvider";
+
+export default function Layout() {
+  const navItems = [
+  {
+    "name": "Home",
+    "href": "#hero"
+  },
+  {
+    "name": "Experience",
+    "href": "#about"
+  },
+  {
+    "name": "Testimonials",
+    "href": "#testimonials"
+  },
+  {
+    "name": "Contact",
+    "href": "#contact"
+  },
+  {
+    "name": "Features",
+    "href": "#features"
+  },
+  {
+    "name": "Metrics",
+    "href": "#metrics"
+  },
+  {
+    "name": "Social Proof",
+    "href": "#social-proof"
+  }
+];
+
+  return (
+    <StyleProvider buttonVariant="elastic" siteBackground="noise" heroBackground="cornerGlow">
+      <SiteBackgroundSlot />
+      <SectionErrorBoundary name="navbar">
+        <NavbarCentered
+      logo="Safin Nashville"
+      ctaButton={{
+        text: "Book Now",
+        href: "#contact",
+      }}
+     navItems={navItems} />
+      </SectionErrorBoundary>
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <SectionErrorBoundary name="footer">
+        <FooterMinimal
+      brand="Safin Luxury Car Service"
+      copyright="© 2024 Safin Nashville. All rights reserved."
+      socialLinks={[
+        {
+          icon: Instagram,
+          href: "#",
+        },
+        {
+          icon: Twitter,
+          href: "#",
+        },
+      ]}
+    />
+      </SectionErrorBoundary>
+    </StyleProvider>
+  );
+}
